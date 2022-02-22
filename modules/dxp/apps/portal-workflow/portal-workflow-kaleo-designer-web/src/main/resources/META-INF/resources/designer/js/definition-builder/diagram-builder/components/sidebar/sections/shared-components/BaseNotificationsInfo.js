69ef50a2d4b98937672890fc658d96b3c4039626
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-02-22 18:34:25 -0300

    LPS-147480 Create generic BaseNotificationsInfo component

---
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/shared-components/BaseNotificationsInfo.js
@@ -0,0 +1,314 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import ClayForm, {ClayInput, ClaySelect} from '@clayui/form';
+import PropTypes from 'prop-types';
+import React, {useContext, useState} from 'react';
+
+import {DiagramBuilderContext} from '../../../../DiagramBuilderContext';
+import ScriptInput from '../../../shared-components/ScriptInput';
+import SidebarPanel from '../../SidebarPanel';
+import Role from '../notifications/Role';
+import RoleType from '../notifications/RoleType';
+import User from '../notifications/User';
+
+const executionTypeOptions = [
+	{
+		label: Liferay.Language.get('on-assignment'),
+		value: 'onAssignment',
+	},
+	{
+		label: Liferay.Language.get('on-entry'),
+		value: 'onEntry',
+	},
+	{
+		label: Liferay.Language.get('on-exit'),
+		value: 'onExit',
+	},
+];
+
+const notificationsTypeOptions = [
+	{
+		label: Liferay.Language.get('email'),
+		value: 'email',
+	},
+	{
+		label: Liferay.Language.get('user-notification'),
+		value: 'userNotification',
+	},
+];
+
+let recipientTypeOptions = [
+	{
+		label: Liferay.Language.get('asset-creator'),
+		value: 'assetCreator',
+	},
+	{
+		label: Liferay.Language.get('role'),
+		value: 'role',
+	},
+	{
+		disabled: true,
+		label: Liferay.Language.get('role-type'),
+		value: 'roleType',
+	},
+	{
+		label: Liferay.Language.get('scripted-recipient'),
+		value: 'scriptedRecipient',
+	},
+	{
+		label: Liferay.Language.get('user'),
+		value: 'user',
+	},
+];
+
+const templateLanguageOptions = [
+	{
+		label: Liferay.Language.get('freemarker'),
+		value: 'freeMarker',
+	},
+	{
+		label: Liferay.Language.get('text'),
+		value: 'text',
+	},
+	{
+		label: Liferay.Language.get('velocity'),
+		value: 'velocity',
+	},
+];
+
+const notificationTypeComponents = {
+	role: Role,
+	roleType: RoleType,
+	scriptedRecipient: ScriptInput,
+	user: User,
+};
+
+const BaseNotificationsInfo = ({
+	index,
+	setSections,
+	updateSelectedItem,
+	...restProps
+}) => {
+	const {selectedItem} = useContext(DiagramBuilderContext);
+	const [executionType, setExecutionType] = useState('');
+	const [notificationDescription, setNotificationDescription] = useState('');
+	const [notificationName, setNotificationName] = useState('');
+	const [notificationType, setNotificationType] = useState('');
+	const [recipientType, setRecipientType] = useState('assetCreator');
+	const [internalSections, setInternalSections] = useState([
+		{identifier: `${Date.now()}-0`},
+	]);
+	const [template, setTemplate] = useState('');
+	const [templateLanguage, setTemplateLanguage] = useState('');
+
+	const selectRecipientType = (item) => {
+		setSections((prev) => {
+			prev[index] = {
+				...prev[index],
+				...item,
+			};
+
+			updateSelectedItem(prev);
+
+			return prev;
+		});
+
+		setRecipientType(item.recipientType);
+	};
+
+	if (
+		selectedItem?.type === 'task' &&
+		!recipientTypeOptions
+			.map((option) => option.value)
+			.includes('taskAssignees')
+	) {
+		recipientTypeOptions.push({
+			label: Liferay.Language.get('task-assignees'),
+			value: 'taskAssignees',
+		});
+	}
+	else if (selectedItem?.type !== 'task') {
+		recipientTypeOptions = recipientTypeOptions.filter(({value}) => {
+			return value !== 'taskAssignees';
+		});
+	}
+
+	const NotificationTypeComponent = notificationTypeComponents[recipientType];
+
+	return (
+		<>
+			<ClayForm.Group>
+				<label htmlFor="notificationName">
+					{Liferay.Language.get('name')}
+				</label>
+
+				<ClayInput
+					id="notificationName"
+					onChange={({target}) => setNotificationName(target.value)}
+					placeholder={Liferay.Language.get('notification')}
+					type="text"
+					value={notificationName}
+				/>
+			</ClayForm.Group>
+
+			<ClayForm.Group>
+				<label htmlFor="notificationDescription">
+					{Liferay.Language.get('description')}
+				</label>
+
+				<ClayInput
+					id="notificationDescription"
+					onChange={({target}) =>
+						setNotificationDescription(target.value)
+					}
+					type="text"
+					value={notificationDescription}
+				/>
+			</ClayForm.Group>
+
+			<ClayForm.Group>
+				<label htmlFor="template-language">
+					{Liferay.Language.get('template-language')}
+				</label>
+
+				<ClaySelect
+					aria-label="Select"
+					id="template-language"
+					onChange={({target}) => setTemplateLanguage(target.value)}
+				>
+					{templateLanguageOptions.map((item) => (
+						<ClaySelect.Option
+							key={item.value}
+							label={item.label}
+							value={item.value}
+						/>
+					))}
+				</ClaySelect>
+			</ClayForm.Group>
+
+			<ClayForm.Group>
+				<label htmlFor="template">
+					{Liferay.Language.get('template')}
+				</label>
+
+				<ClayInput
+					component="textarea"
+					id="template"
+					onChange={({target}) => setTemplate(target.value)}
+					placeholder="${userName} sent you a ${entryType} for review in the workflow."
+					type="text"
+					value={template}
+				/>
+			</ClayForm.Group>
+
+			<ClayForm.Group>
+				<label htmlFor="notifications-type">
+					{Liferay.Language.get('notifications-type')}
+				</label>
+
+				<ClaySelect
+					aria-label="Select"
+					defaultValue={Liferay.Language.get('select')}
+					id="notifications-type"
+					onChange={({target}) => setNotificationType(target.value)}
+				>
+					{notificationsTypeOptions.map((item) => (
+						<ClaySelect.Option
+							key={item.value}
+							label={item.label}
+							value={item.value}
+						/>
+					))}
+				</ClaySelect>
+			</ClayForm.Group>
+
+			<ClayForm.Group>
+				<label htmlFor="execution-type">
+					{Liferay.Language.get('execution-type')}
+				</label>
+
+				<ClaySelect
+					aria-label="Select"
+					id="execution-type"
+					onChange={({target}) => setExecutionType(target.value)}
+				>
+					{executionTypeOptions.map((item) => (
+						<ClaySelect.Option
+							key={item.value}
+							label={item.label}
+							value={item.value}
+						/>
+					))}
+				</ClaySelect>
+			</ClayForm.Group>
+
+			<ClayForm.Group className="recipient-type-form-group">
+				<label htmlFor="recipient-type">
+					{Liferay.Language.get('recipient-type')}
+				</label>
+
+				<ClaySelect
+					aria-label="Select"
+					id="recipient-type"
+					onChange={({target}) => setRecipientType(target.value)}
+					onClickCapture={() =>
+						selectRecipientType({
+							description: notificationDescription,
+							executionType,
+							name: notificationName,
+							notificationType,
+							recipientType,
+							template,
+							templateLanguage,
+						})
+					}
+				>
+					{recipientTypeOptions.map((item) => (
+						<ClaySelect.Option
+							disabled={item.disabled}
+							key={item.value}
+							label={item.label}
+							value={item.value}
+						/>
+					))}
+				</ClaySelect>
+			</ClayForm.Group>
+
+			{recipientType !== 'assetCreator' &&
+				recipientType !== 'taskAssignees' && (
+					<SidebarPanel panelTitle={Liferay.Language.get('type')}>
+						<ClayForm.Group className="recipient-type-form-group">
+							{internalSections.map(({identifier}, index) => (
+								<NotificationTypeComponent
+									identifier={identifier}
+									index={index}
+									inputValue=""
+									key={`section-${identifier}`}
+									sectionsLength={internalSections.length}
+									setSections={setInternalSections}
+									{...restProps}
+								/>
+							))}
+						</ClayForm.Group>
+					</SidebarPanel>
+				)}
+		</>
+	);
+};
+
+BaseNotificationsInfo.propTypes = {
+	index: PropTypes.number.isRequired,
+	setSections: PropTypes.func.isRequired,
+	updateSelectedItem: PropTypes.func.isRequired,
+};
+
+export default BaseNotificationsInfo;
