7037c7e5c27765208691f06fdae144eb24ab1f7d
Laszlo Pap <laszlo.pap@liferay.com>
2022-04-21 15:24:35 +0200

    LPS-151947 Consider the user's timezone for allday long calendar events

--- modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateContextFactory.java
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/notification/NotificationTemplateContextFactory.java
@@ -99,8 +99,7 @@ public class NotificationTemplateContextFactory {
 
 		// Attributes
 
-		Format userDateTimeFormat = _getUserDateTimeFormat(
-			calendarBooking, user);
+		Format userDateTimeFormat = _getUserDateTimeFormat(user);
 
 		String userTimezoneDisplayName = _getUserTimezoneDisplayName(user);
 
@@ -196,8 +195,7 @@ public class NotificationTemplateContextFactory {
 			long instanceStartTime = (long)serviceContext.getAttribute(
 				"instanceStartTime");
 
-			Format userDateTimeFormat = _getUserDateTimeFormat(
-				calendarBooking, user);
+			Format userDateTimeFormat = _getUserDateTimeFormat(user);
 
 			String userTimezoneDisplayName = _getUserTimezoneDisplayName(user);
 
@@ -280,14 +278,8 @@ public class NotificationTemplateContextFactory {
 		return company.getPortalURL(groupId);
 	}
 
-	private static Format _getUserDateTimeFormat(
-		CalendarBooking calendarBooking, User user) {
-
+	private static Format _getUserDateTimeFormat(User user) {
 		TimeZone userTimeZone = user.getTimeZone();
-
-		if ((calendarBooking != null) && calendarBooking.isAllDay()) {
-			userTimeZone = TimeZone.getTimeZone(StringPool.UTC);
-		}
 
 		return FastDateFormatFactoryUtil.getDateTime(
 			user.getLocale(), userTimeZone);
