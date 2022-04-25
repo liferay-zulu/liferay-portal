e7f839543238bf2e4b9c3e10138e5b073601f6e9
MariMalta <maltamariana123@gmail.com>
2022-04-25 17:54:34 -0300

    LPS-151713/LPS-151714 Create Sidebar Componet
    Co-authored-by: Carlos Montenegro <cgmonte@gmail.com>

---
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/Editor/Sidebar/Sidebar.tsx
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import React from 'react';
+
+import {
+	ObjectValidationRuleElement,
+	ObjectValidationRuleElementItem,
+} from '../../..';
+
+import '../Editor.scss';
+
+import './Sidebar.scss';
+import {METADATAS} from '../../ObjectView/context';
+import {Collapsable} from './Collapsable';
+import Element from './Element';
+
+export default function Sidebar({
+	inputChannel,
+	objectValidationRuleElements,
+}: IProps) {
+	const onItemClick = (item: ObjectValidationRuleElementItem) =>
+		inputChannel.sendData(item.content);
+
+	const defaultLanguageId = Liferay.ThemeDisplay.getDefaultLanguageId();
+
+	const metadatas = METADATAS.map((metadata) => ({
+		content: metadata.name,
+		label: metadata.label[defaultLanguageId],
+		tooltip: 'placeholder',
+	}));
+
+	objectValidationRuleElements[0].items = objectValidationRuleElements[0].items.concat(
+		metadatas
+	);
+
+	return (
+		<div className="lfr-objects__object-editor-sidebar">
+			<div className="px-3">
+				<h5 className="my-3">{Liferay.Language.get('elements')}</h5>
+
+				{objectValidationRuleElements.map(({items, label}) => (
+					<Collapsable key={label} label={label}>
+						<Element items={items} onItemClick={onItemClick} />
+					</Collapsable>
+				))}
+			</div>
+		</div>
+	);
+}
+interface IProps {
+	className?: string;
+	inputChannel: {sendData: Function};
+	objectValidationRuleElements: ObjectValidationRuleElement[];
+}
