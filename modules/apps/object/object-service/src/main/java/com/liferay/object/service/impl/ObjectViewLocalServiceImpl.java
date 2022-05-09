4422710e1d519fad70f281a1edfd840ddae62b5d
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-05-09 19:51:02 -0300

    LPS-152196 Adapt date columns to be filterable

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectViewLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectViewLocalServiceImpl.java
@@ -440,9 +440,11 @@ public class ObjectViewLocalServiceImpl extends ObjectViewLocalServiceBaseImpl {
 			if (_objectFieldNames.contains(
 					objectViewFilterColumn.getObjectFieldName())) {
 
-				if (!Objects.equals(
+				if (Objects.equals(
 						objectViewFilterColumn.getObjectFieldName(),
-						"status")) {
+						"creator") ||
+					Objects.equals(
+						objectViewFilterColumn.getObjectFieldName(), "id")) {
 
 					throw new ObjectViewFilterColumnException(
 						StringBundler.concat(
@@ -457,6 +459,9 @@ public class ObjectViewLocalServiceImpl extends ObjectViewLocalServiceBaseImpl {
 					objectViewFilterColumn.getObjectFieldName());
 
 				if (!Objects.equals(
+						objectField.getBusinessType(),
+						ObjectFieldConstants.BUSINESS_TYPE_DATE) &&
+					!Objects.equals(
 						objectField.getBusinessType(),
 						ObjectFieldConstants.BUSINESS_TYPE_PICKLIST)) {
 
@@ -485,7 +490,7 @@ public class ObjectViewLocalServiceImpl extends ObjectViewLocalServiceBaseImpl {
 					StringBundler.concat(
 						"Object field name \"",
 						objectViewFilterColumn.getObjectFieldName(),
-						"\" needs to have the fitler type and JSON specified"));
+						"\" needs to have the filter type and JSON specified"));
 			}
 
 			ObjectFieldFilterParser objectFieldFilterParser =
