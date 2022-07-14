b9058b0a21e29faee60a9cf78e31d292b259323e
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-07-14 17:34:50 -0300

    LPS-157925 Complete test cases of ObjectStateLocalServiceTest

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectStateLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectStateLocalServiceTest.java
@@ -19,19 +19,23 @@ import com.liferay.list.type.model.ListTypeDefinition;
 import com.liferay.list.type.model.ListTypeEntry;
 import com.liferay.list.type.service.ListTypeDefinitionLocalService;
 import com.liferay.list.type.service.ListTypeEntryLocalService;
-import com.liferay.object.exception.NoSuchObjectStateException;
-import com.liferay.object.model.ObjectState;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectStateFlow;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.ObjectStateFlowLocalService;
 import com.liferay.object.service.ObjectStateLocalService;
 import com.liferay.object.util.LocalizedMapUtil;
-import com.liferay.object.util.ObjectFieldBuilder;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -39,11 +43,14 @@ import com.liferay.portal.util.PropsUtil;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -60,13 +67,24 @@ public class ObjectStateLocalServiceTest {
 	public static final AggregateTestRule aggregateTestRule =
 		new LiferayIntegrationTestRule();
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUpClass() {
 		PropsUtil.addProperties(
 			UnicodePropertiesBuilder.setProperty(
 				"feature.flag.LPS-152677", "true"
 			).build());
+	}
 
+	@AfterClass
+	public static void tearDownClass() {
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-152677", "false"
+			).build());
+	}
+
+	@Before
+	public void setUp() throws Exception {
 		_listTypeDefinition =
 			_listTypeDefinitionLocalService.addListTypeDefinition(
 				TestPropsValues.getUserId(),
@@ -75,178 +93,82 @@ public class ObjectStateLocalServiceTest {
 
 		_listTypeEntries = new ArrayList<>();
 
-		for (String key : Arrays.asList("closed", "open", "review")) {
+		for (String key : Arrays.asList("step1", "step2", "step3")) {
 			_listTypeEntries.add(
 				_listTypeEntryLocalService.addListTypeEntry(
 					TestPropsValues.getUserId(),
 					_listTypeDefinition.getListTypeDefinitionId(), key,
 					LocalizedMapUtil.getLocalizedMap(key)));
 		}
+
+		_objectDefinition =
+			_objectDefinitionLocalService.addCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				"A" + RandomTestUtil.randomString(), null, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				ObjectDefinitionConstants.SCOPE_COMPANY,
+				ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT,
+				Collections.emptyList());
+
+		_objectStateFlow = _addDefaultObjectStateFlow();
 	}
 
 	@After
-	public void tearDown() {
-		PropsUtil.addProperties(
-			UnicodePropertiesBuilder.setProperty(
-				"feature.flag.LPS-152677", "false"
-			).build());
+	public void tearDown() throws PortalException {
+		_objectDefinitionLocalService.deleteObjectDefinition(_objectDefinition);
 	}
 
 	@Test
 	public void testAddObjectState() throws PortalException {
-		long listTypeEntry = RandomTestUtil.randomLong();
-		long objectStateFlowId = RandomTestUtil.randomLong();
+		ListTypeEntry listTypeEntry =
+			_listTypeEntryLocalService.addListTypeEntry(
+				TestPropsValues.getUserId(),
+				_listTypeDefinition.getListTypeDefinitionId(), "step4",
+				LocalizedMapUtil.getLocalizedMap("step4"));
 
-		ObjectState objectState = _testAddObjectState(
-			listTypeEntry, objectStateFlowId, TestPropsValues.getUserId());
-
-		Assert.assertEquals(
-			TestPropsValues.getUserId(), objectState.getUserId());
-		Assert.assertEquals(
-			TestPropsValues.getCompanyId(), objectState.getCompanyId());
-		Assert.assertEquals(listTypeEntry, objectState.getListTypeEntryId());
-		Assert.assertEquals(
-			objectStateFlowId, objectState.getObjectStateFlowId());
+		Assert.assertNotNull(
+			_objectStateLocalService.getObjectStateFlowObjectState(
+				listTypeEntry.getListTypeEntryId(),
+				_objectStateFlow.getObjectStateFlowId()));
 	}
 
 	@Test
 	public void testDeleteListTypeEntryObjectStates() throws PortalException {
-		ObjectState objectState = _testAddObjectState(
-			RandomTestUtil.randomLong(), RandomTestUtil.randomLong(),
-			TestPropsValues.getUserId());
+		ListTypeEntry listTypeEntry = _listTypeEntries.get(0);
 
-		Assert.assertNotNull(objectState);
-
-		_objectStateLocalService.deleteListTypeEntryObjectStates(
-			objectState.getListTypeEntryId());
+		_listTypeEntryLocalService.deleteListTypeEntry(
+			listTypeEntry.getListTypeEntryId());
 
 		try {
-			_objectStateLocalService.getObjectState(
-				objectState.getObjectStateId());
+			_objectStateLocalService.getObjectStateFlowObjectState(
+				listTypeEntry.getListTypeEntryId(),
+				_objectStateFlow.getObjectStateFlowId());
 
 			Assert.fail();
 		}
-		catch (NoSuchObjectStateException noSuchObjectStateException) {
+		catch (PortalException portalException) {
 			Assert.assertEquals(
-				"No ObjectState exists with the primary key " +
-					objectState.getObjectStateId(),
-				noSuchObjectStateException.getMessage());
+				portalException.getMessage(),
+				StringBundler.concat(
+					"No ObjectState exists with the key {listTypeEntryId=",
+					listTypeEntry.getListTypeEntryId(), ", objectStateFlowId=",
+					_objectStateFlow.getObjectStateFlowId(), "}"));
 		}
 	}
 
-	@Test
-	public void testDeleteObjectStateFlowObjectStates() throws PortalException {
-		ObjectStateFlow objectStateFlow = _addDefaultObjectStateFlow();
+	private ObjectStateFlow _addDefaultObjectStateFlow() throws Exception {
+		ObjectField objectField = _objectFieldLocalService.addCustomObjectField(
+			TestPropsValues.getUserId(),
+			_listTypeDefinition.getListTypeDefinitionId(),
+			_objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_PICKLIST,
+			ObjectFieldConstants.DB_TYPE_STRING, null, false, true, "",
+			LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+			StringUtil.randomId(), true, true, Collections.emptyList());
 
-		_objectStateLocalService.deleteObjectStateFlowObjectStates(
-			objectStateFlow.getObjectStateFlowId());
-
-		for (ListTypeEntry listTypeEntry : _listTypeEntries) {
-			try {
-				_objectStateLocalService.getObjectStateFlowObjectState(
-					listTypeEntry.getListTypeEntryId(),
-					objectStateFlow.getObjectStateFlowId());
-
-				Assert.fail();
-			}
-			catch (NoSuchObjectStateException noSuchObjectStateException) {
-				Assert.assertEquals(
-					StringBundler.concat(
-						"No ObjectState exists with the key {listTypeEntryId=",
-						listTypeEntry.getListTypeEntryId(),
-						", objectStateFlowId=",
-						objectStateFlow.getObjectStateFlowId(), "}"),
-					noSuchObjectStateException.getMessage());
-			}
-		}
-	}
-
-	@Test
-	public void testGetNextObjectStates() throws PortalException {
-		ObjectStateFlow objectStateFlow = _addDefaultObjectStateFlow();
-
-		for (ListTypeEntry listTypeEntry : _listTypeEntries) {
-			List<ObjectState> nextObjectStates = new ArrayList<>();
-
-			for (ListTypeEntry listTypeEntry1 : _listTypeEntries) {
-				if (listTypeEntry1.getListTypeEntryId() ==
-						listTypeEntry.getListTypeEntryId()) {
-
-					continue;
-				}
-
-				nextObjectStates.add(
-					_objectStateLocalService.getObjectStateFlowObjectState(
-						listTypeEntry1.getListTypeEntryId(),
-						objectStateFlow.getObjectStateFlowId()));
-			}
-
-			ObjectState sourceObjectState =
-				_objectStateLocalService.getObjectStateFlowObjectState(
-					listTypeEntry.getListTypeEntryId(),
-					objectStateFlow.getObjectStateFlowId());
-
-			Assert.assertEquals(
-				nextObjectStates,
-				_objectStateLocalService.getNextObjectStates(
-					sourceObjectState.getObjectStateId()));
-		}
-	}
-
-	@Test
-	public void testGetObjectStateFlowObjectState() throws PortalException {
-		long listTypeEntryId = RandomTestUtil.randomLong();
-		long objectStateFlowId = RandomTestUtil.randomLong();
-
-		ObjectState objectState = _testAddObjectState(
-			listTypeEntryId, objectStateFlowId, TestPropsValues.getUserId());
-
-		Assert.assertEquals(
-			objectState,
-			_objectStateLocalService.getObjectStateFlowObjectState(
-				listTypeEntryId, objectStateFlowId));
-	}
-
-	@Test
-	public void testGetObjectStateFlowObjectStates() throws PortalException {
-		ObjectStateFlow objectStateFlow = _addDefaultObjectStateFlow();
-
-		List<ObjectState> objectStates = new ArrayList<>();
-
-		for (ListTypeEntry listTypeEntry : _listTypeEntries) {
-			objectStates.add(
-				_objectStateLocalService.getObjectStateFlowObjectState(
-					listTypeEntry.getListTypeEntryId(),
-					objectStateFlow.getObjectStateFlowId()));
-		}
-
-		Assert.assertEquals(
-			objectStates,
-			_objectStateLocalService.getObjectStateFlowObjectStates(
-				objectStateFlow.getObjectStateFlowId()));
-	}
-
-	private ObjectStateFlow _addDefaultObjectStateFlow()
-		throws PortalException {
-
-		ObjectFieldBuilder objectFieldBuilder = new ObjectFieldBuilder();
-
-		return _objectStateFlowLocalService.addDefaultObjectStateFlow(
-			objectFieldBuilder.userId(
-				TestPropsValues.getUserId()
-			).listTypeDefinitionId(
-				_listTypeDefinition.getListTypeDefinitionId()
-			).state(
-				true
-			).build());
-	}
-
-	private ObjectState _testAddObjectState(
-			long listTypeEntryId, long objectStateFlowId, long userId)
-		throws PortalException {
-
-		return _objectStateLocalService.addObjectState(
-			userId, listTypeEntryId, objectStateFlowId);
+		return _objectStateFlowLocalService.getObjectFieldObjectStateFlow(
+			objectField.getObjectFieldId());
 	}
 
 	@DeleteAfterTestRun
@@ -255,11 +177,20 @@ public class ObjectStateLocalServiceTest {
 	@Inject
 	private ListTypeDefinitionLocalService _listTypeDefinitionLocalService;
 
-	@DeleteAfterTestRun
 	private List<ListTypeEntry> _listTypeEntries;
 
 	@Inject
 	private ListTypeEntryLocalService _listTypeEntryLocalService;
+
+	private ObjectDefinition _objectDefinition;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Inject
+	private ObjectFieldLocalService _objectFieldLocalService;
+
+	private ObjectStateFlow _objectStateFlow;
 
 	@Inject
 	private ObjectStateFlowLocalService _objectStateFlowLocalService;
